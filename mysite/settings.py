3e2946a83949a73e6abd1f7c8775ce85a1db41b7
IFaniksI <nikitazhuro62@gmail.com>
2024-01-09 21:47:05 +0300

    Release

--- mysite/settings.py
+++ mysite/settings.py
@@ -75,10 +75,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'django',
+       'USER': 'django',
+       'PASSWORD': 'django',
+       'HOST': 'localhost',
+       'PORT': 5432,
+   }
 }
 
 
